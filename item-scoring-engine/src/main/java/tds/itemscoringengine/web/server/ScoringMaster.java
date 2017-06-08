3caae4877a27be2a4ec1ded4f4c26c75da178f6e
Todd Miller <millerts@users.noreply.github.com>
2017-06-08 03:26:32 -0700

    Merge3.1.0.release (#11)
    * Created new branch Integration
    
    * merging sbac internal snapshot versions to release integration branch
    
    * Logstashing (#2)
    
    Log all entry and exit points to logstash with all parameters and any exceptions.
    
    
    Add exception output to logging, make logging output match java apps.
    
    Put response parsing into try blocks so we print and log their exceptions instead of skipping exit log and failing to screen.
    
    Administrivia: Add a .gitignore and a versioned requirements.txt, convert file dos2unix, fix some formatting to somewhat resemble PEP8 (remove tabs!).
    
    * Bump parent POM dependency to released version.  Clean up POMs.
    
    * Fix tests
    
    * [maven-release-plugin] prepare release 3.0.0
    
    * [maven-release-plugin] prepare for next development iteration
    
    * Preparing for release 3.0.0
    
    * Elimination of Unused Dependencies – SB-1509 (#1)
    
    * README.md edited online with Bitbucket
    
    * Added gitignore
    
    * Update tag file fw-tags.txt
    
    * Changes done to remove neo4j jar dependency, as it is not used , changes
    for JIRA # SB-1509,
    
    * Update pom.xml
    
    Removed Commented portion of pom.xml for neo4j.xml
    
    * Update parent pom to latest version.
    Add source jar generation to the release profile.
    
    * Add a module to create a docker container image for the EqScoringWebservice python service.
    
    * Removed unused log directory creation from Dockerfile.
    
    * Add a docker container definition to the item-scoring-service WAR.
    
    * Feature/inject dependencies (#6)
    
    * Remove intellij files from being tracked by git
    
    * Allow injection of the HttpWebHelper
    
    * Pass in dependencies rather than reaching out for them through static accessors that actually require a Spring container configured a particular way.
    
    * Release - Contains injection of properties (#7)
    
    * [maven-release-plugin] prepare release 3.0.1
    
    * [maven-release-plugin] prepare for next development iteration
    
    * Bump shared-master to latest version.
    
    * Bump container image to use latest secure-tomcat
    
    * Send logs to log stash if LOGSTASH_DESTINATION env var is defined
    
    * Prepare release (#9)
    
    * README.md edited online with Bitbucket
    
    * Added gitignore
    
    * Update tag file fw-tags.txt
    
    * update shared-master to release version
    
    * [maven-release-plugin] prepare release 3.1.0.RELEASE

--- item-scoring-engine/src/main/java/tds/itemscoringengine/web/server/ScoringMaster.java
+++ item-scoring-engine/src/main/java/tds/itemscoringengine/web/server/ScoringMaster.java
@@ -1,32 +1,30 @@
 /*******************************************************************************
  * Educational Online Test Delivery System Copyright (c) 2014 American
  * Institutes for Research
- * 
+ *
  * Distributed under the AIR Open Source License, Version 1.0 See accompanying
  * file AIR-License-1_0.txt or at http://www.smarterapp.org/documents/
  * American_Institutes_for_Research_Open_Source_Software_License.pdf
  ******************************************************************************/
 package tds.itemscoringengine.web.server;
 
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Map;
-
-import org.apache.commons.lang.StringUtils;
+import AIR.Common.Configuration.AppSettingsHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import qtiscoringengine.CustomOperatorRegistry;
 import qtiscoringengine.ISECustomOperator;
 import qtiscoringengine.QTIScoringRuntimeException;
-import tds.itemscoringengine.IItemScorer;
-import tds.itemscoringengine.ItemScorerManagerImpl;
 import tinyctrlscoringengine.CtrlCustomOpFactory;
 import tinyequationscoringengine.TinyEqCustomOpFactory;
 import tinygrscoringengine.TinyGRCustomOpFactory;
 import tinytablescoringengine.TinyTableCustomOpFactory;
-import AIR.Common.Configuration.AppSettingsHelper;
-import AIR.Common.Utilities.SpringApplicationContext;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import tds.itemscoringengine.IItemScorer;
+import tds.itemscoringengine.ItemScorerManagerImpl;
 
 // Note: the Spring configuration for this component is in the
 // servlet-context.xml
@@ -37,15 +35,34 @@ public class ScoringMaster extends ItemScorerManagerImpl
   private static final Logger _logger = LoggerFactory.getLogger (ScoringMaster.class);
 
   public ScoringMaster (Map<String, IItemScorer> engines, AppStatsRecorder statsRecorder) {
-    super (AppSettingsHelper.getInt32 ("QThreadCount", 20), AppSettingsHelper.getInt32 ("QHiWaterMark", 500), AppSettingsHelper.getInt32 ("QLowWaterMark", 400), statsRecorder);
+    this(engines,
+      statsRecorder,
+      AppSettingsHelper.getInt32("QThreadCount", 20),
+      AppSettingsHelper.getInt32("QHiWaterMark", 500),
+      AppSettingsHelper.getInt32("QLowWaterMark", 400),
+      getPythonScoringUrl(),
+      getMaxTries(),
+      AppSettingsHelper.getInt32 ("itemscoring.qti.sympyTimeoutMillis", 10000));
+  }
 
+  public ScoringMaster(
+    final Map<String, IItemScorer> engines,
+    final AppStatsRecorder statsRecorder,
+    final int qThreadCount,
+    final int qHiWaterMark,
+    final int qLowWaterMark,
+    final String pythonScoringUrl,
+    final int maxAttempts,
+    final int timeoutInMillis) {
+
+    super (qThreadCount, qHiWaterMark, qLowWaterMark, statsRecorder);
     statsRecorder.setCreationTime ();
 
     for (Map.Entry<String, IItemScorer> entry_i : engines.entrySet ()) {
       RegisterItemScorer (entry_i.getKey (), entry_i.getValue ());
     }
 
-    initOperators ();
+    initOperators(pythonScoringUrl, maxAttempts, timeoutInMillis);
   }
 
   private static String getPythonScoringUrl () {
@@ -57,13 +74,13 @@ public class ScoringMaster extends ItemScorerManagerImpl
   }
 
   // TODO Shiva: We need to ensure that initOperators may be invoked only ones.
-  private void initOperators () {
+  private void initOperators(String pythonScoringUrl, int maxAttempts, int timeoutInMillis) {
     CustomOperatorRegistry.getInstance ().addOperatorFactory (new ISECustomOperator (this));
     CustomOperatorRegistry.getInstance ().addOperatorFactory (new TinyGRCustomOpFactory ());
     CustomOperatorRegistry.getInstance ().addOperatorFactory (new CtrlCustomOpFactory ());
     CustomOperatorRegistry.getInstance ().addOperatorFactory (new TinyTableCustomOpFactory ());
     try {
-      CustomOperatorRegistry.getInstance ().addOperatorFactory (new TinyEqCustomOpFactory (new URI (getPythonScoringUrl ()), getMaxTries ()));
+      CustomOperatorRegistry.getInstance ().addOperatorFactory (new TinyEqCustomOpFactory (new URI (pythonScoringUrl), maxAttempts, timeoutInMillis));
     } catch (URISyntaxException exp) {
       throw new QTIScoringRuntimeException (exp);
     }
--- sympy-scripts/README.txt
+++ sympy-scripts/README.txt
@@ -1,19 +1,22 @@
 As mentioned in "Running Item Scoring Engine.docx" file, 
 EqScoringWebService.py is the python scoring script.
 
-However, it needs cherrypy and sympy. Please see the Word document
-for more details on version of cherrypy. You will have to install
-the required version of cherrypy from the official website.
-We will not discuss cherrypy further here.
-
-Sympy however, has been customized by AIR.
-
-For the customized version of sympy there are no installation files/folders.
-You will see a zip file called site-packages.zip. 
-
-
 You will need Python-2.7.  
-If you have installed Python into c:\python27 folder then you 
-will see a subfolder C:\install\Python27\Lib\site-packages.
 
-Copy the contents of the unzipped site-packages.zip in the the folder mentioned above.
+Install requirements from requirements.txt file as follows:
+$ sudo -H pip install -r requirements.txt
+
+Sympy has been customized by AIR and is in the site-packages.zip file.
+
+You can install it on the system, or you can run it as follows by setting
+PYTHONPATH to the egg file containing the modded Sympy:
+
+$ export PYTHONPATH=`pwd`/site-packages.zip
+$ python EqScoringWebService.py
+
+Windows instructions:
+  For the customized version of sympy there are no installation files/folders.
+  You will see a zip file called site-packages.zip. 
+  
+  Assuming you have installed Python into C:\Python27, simply unzip the
+  contents of site-packages.zip into 'C:\Python27\Lib\site-packages'.
